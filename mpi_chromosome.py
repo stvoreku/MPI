7c8816f747defb38978b22695ac0414665af7901
Jacek Orlinski <stvoreku@gmail.com>
2022-01-30 22:50:56 +0100

    Genetics

---
+++ mpi_chromosome.py
@@ -0,0 +1,53 @@
+import numpy as np
+import random
+
+class Chromosome():
+
+    def __init__(self, m, n, c):
+        self.X = np.zeros(shape=(m, n), dtype=int)
+        self.M = m
+        self.N = n
+        self.C = c
+
+    def __str__(self):
+        return f"{str(self.X)} \n"
+
+    @property
+    def cost(self):
+        return (self.C * self.X).sum()
+
+    def feasible(self, A, B):
+        usage = B - (A * self.X).sum(axis=1)
+        return all(i >= 0 for i in usage)
+
+    def from_X(self, X):
+        self.X = X
+
+    def random_feasible(self, A, B):
+        print("Generating random feasible solution")
+        used_trial = [-1]
+        while any(i <= 0 for i in used_trial):
+            trial_X = np.zeros(shape=(self.M, self.N), dtype=int)
+            for x in trial_X.T:
+                r = random.randint(0, 4)
+                x[r] = 1
+            used_trial = B - (A * trial_X).sum(axis=1)
+        self.X = trial_X
+
+    def mix_genes(self, parents):
+        for i in range(0, len(self.X.T)):
+            p_index = random.randint(0,1)
+            self.X.T[i] = parents[p_index].X.T[i]
+    def mutate(self):
+        for i in range(0, len(self.X.T)):
+            mut_p = random.randint(0,8)
+            if mut_p == 1:
+                inte = random.randint(0,4)
+                self.X.T[i] = np.roll(self.X.T[i], inte)
+    def usages(self, A, B):
+        return B - (A * self.X).sum(axis=1)
+
+    def __repr__(self):
+        return f'{self.cost}'
+
+
---
+++ mpi_genetic.py
@@ -0,0 +1,60 @@
+import numpy as np
+import random
+from mpi_chromosome import Chromosome
+
+m = 5 #Liczba serwerów MEC
+n = 15 #Liczba zadań do przypisania
+
+b = [36,34,38,27,33] #Pojemności dla każdego mec
+#b = [35,35,35,35,35]
+
+a = np.asarray([
+    8,15,14,23, 8,16, 8,25, 9,17,25,15,10, 8,24,
+    15, 7,23,22,11,11,12,10,17,16, 7,16,10,18,22,
+    21,20, 6,22,24,10,24, 9,21,14,11,14,11,19,16,
+    20,11, 8,14, 9, 5, 6,19,19, 7, 6, 6,13, 9,18,
+    8,13,13,13,10,20,25,16,16,17,10,10, 5,12,23]).reshape(m,n)
+
+c = np.asarray([
+    17,21,22,18,24,15,20,18,19,18,16,22,24,24,16,
+    23,16,21,16,17,16,19,25,18,21,17,15,25,17,24,
+    16,20,16,25,24,16,17,19,19,18,20,16,17,21,24,
+    19,19,22,22,20,16,19,17,21,19,25,23,25,25,25,
+    18,19,15,15,21,25,16,16,23,15,22,17,19,22,24]).reshape(m,n)
+
+def cross(parents, num_offspring):
+    offsprings_list = []
+    #generujemy zadaną liczbę dzieci
+    while len(offsprings_list) < num_offspring:
+        new_ch = Chromosome(m,n,c)
+        new_ch.mix_genes(parents)
+        new_ch.mutate()
+        if new_ch.feasible(a,b):
+            offsprings_list.append(new_ch)
+    return offsprings_list
+
+
+
+used_trial = [-1]
+print('Looking for 2 solutions for initial pair')
+population = []
+while len(population) < 2:
+    ch = Chromosome(m,n,c)
+    ch.random_feasible(a,b)
+    population.append(ch)
+
+print('Got two parents')
+
+for i in range(0,10000):
+    print(f"Generation {i}")
+    print(population)
+    offs = cross(population, 3)
+    population += offs
+    population.sort(key=lambda x: x.cost, reverse=False)
+    population = population[0:10] #ograniczamy populacje do najlepszych
+    if population[0].cost == 261:
+        break
+
+
+
+
